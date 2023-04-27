287ae050ac5f0ce40905d845db0e9cf625380f54
joaodematejr <joao.demate@alquimidiacorp.com.br>
2023-04-27 10:45:35 -0300

    feat (DTO Create)

---
+++ project/api/src/user/dto/create-user.dto.ts
@@ -0,0 +1,23 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  IsStrongPassword,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  readonly name: string;
+
+  @IsEmail()
+  readonly email: string;
+
+  @IsStrongPassword({
+    minLength: 8,
+    minLowercase: 1,
+    minUppercase: 1,
+    minNumbers: 1,
+  })
+  readonly password: string;
+}
